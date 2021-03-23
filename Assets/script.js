559192a34942b3c3f6c0f8758a939aa8ea90dab5
Asha Sanford <sanford.asha97@yahoo.com>
2021-03-23 13:21:07 -0400

    linked script file. created a variable for the city input. created a function to grab weather data

---
+++ Assets/script.js
@@ -0,0 +1,34 @@
+console.log("script is linked");
+
+var cityInput = document.querySelector("#cityname");
+var searchBtn = document.querySelector("#searchButton")
+
+
+
+searchBtn.addEventListener("click", getCityName);
+
+function getCityName() {
+    var cityName = cityInput.value;
+    console.log(cityName);
+}
+
+var APIKey = "43d8cb855d9885bc04142f8adeb51405";
+
+function getCityWeather(cityName) {
+    var apiUrl = "https://pro.openweathermap.org/data/2.5/forecast/hourly?q={city name}&appid={API key}";
+    var apiUrl2 = "https://pro.openweathermap.org/data/2.5/forecast/hourly?q={atlanta}&appid=" + APIKey;
+    var apiUrl3 = "https://pro.openweathermap.org/data/2.5/forecast/hourly?q=atlanta&appid=" + APIKey;
+    var apiUrl4 = "https://pro.openweathermap.org/data/2.5/forecast/hourly?q={atlanta}&appid={" + APIKey + "}";
+
+    var apiUrl5 = "http://api.openweathermap.org/data/2.5/weather?q=Atlanta&APPID=" + APIKey;
+
+    fetch(apiUrl5)
+    .then(function (response) {
+        return response.json();
+    })
+
+    .then(function (data) {
+        console.log(data)
+    })
+
+}
